b9c239fc27054e7d37089c61864cc6436ad12abd
poiu542 <88714716+poiu542@users.noreply.github.com>
2023-05-24 12:08:16 +0900

    feat : 릴스페이지 구현

--- src/components/reels/Reels2.vue
+++ src/components/reels/Reels2.vue
@@ -1,20 +1,23 @@
 <template>
   <!-- <Header /> -->
+  <div class="player">
   <iframe class="video-box"
           id=""
-          :src="`https://www.youtube.com/embed/vWD48Q8YKc4?autoplay=1`"
+          :src="`https://www.youtube.com/embed/${this.videoId[index]}?rel=0`"
           title="[아이유] 근력 운동을 열심히 한다는 아이유"
           frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
           />
-  <div id="player">
   </div>
+  <button class="btn btn-light next-btn" type="button" @click="next">다음영상</button>  
+
   <Navbar />
 </template>
 
 <script>
 // import Header from '../common/Header.vue'
 import Navbar from '../common/Navbar.vue'
+import axios from 'axios';
 
 export default {
   components: {
@@ -23,26 +26,78 @@ export default {
   },
   data() {
     return {
-      sample:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDdwDEYRPat47IaYdO719kyxDBPCxMEfgIZGEEfreNVg&s",
-      videoId:"vWD48Q8YKc4"
+      index:0,
+      videoId:[]
     }
   },
   methods : {
     next() {
-      this.sample = "sample.mp4"
-      console.log("next")
+      this.index = (this.index + 1) % this.videoId.length;
     }
   },
   created() {
     this.$store.commit("setNavState", 3)
+    const url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=3&status=&playlistId=PLgaOBPAy_5i-xlhbulU-BT_1WpdaKIrxS&key=" + process.env.VUE_APP_YOUTUBE_API_KEY;
+    axios.get(url)
+    .then((result) => {
+      let temp = []
+      for (let item of result.data.items) {
+        temp.push(item.snippet.resourceId.videoId)
+        console.log(item.snippet.resourceId.videoId)
+      }
+      this.videoId = temp;
+    })
   },
 }
 </script>
 
 <style scoped>
+.player{
+  background-color: black;
+  width: 100%;
+  height: 100%;
+}
+.icon {
+  -webkit-filter: invert(100%);
+  filter: invert(100%);
+}
+.footer {
+  background-color: black;
+}
+a {
+  display: hidden !important;
+  visibility: hidden !important;
+}
+.ytp-title {
+  display: hidden !important;
+  visibility: hidden !important;
+}
+.yt-uix-sessionlink{
+  display: hidden !important;
+  visibility: hidden !important;
+}
+.ytp-title-text{
+  display: hidden !important;
+  visibility: hidden !important;
+}
+
+.next-btn {
+  position: absolute;
+  top: 0.5%;
+  right: 1%;
+  font-weight: bold;
+  
+  border-color: #a99df0;
+  background-color: #a99df0;
+  opacity: 0.4;
+}
+.player {
+  width: 100%;
+  height: 100%;
+}
 .video-box {
    width:412px;
-   height:860px;
+   height:862px;
 }
 .video-text {
   color: aliceblue;
